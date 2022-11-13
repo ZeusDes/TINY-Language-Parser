eb42a6c409b05e2581b0cfd6a15fd0af96a55778
ZeusDes <mohamed.ahmed.ma19.ma@gmail.com>
2022-11-13 18:28:49 +0200

    Adding Scanner States

--- src/main/java/com/tinylang/utils/Scanner.java
+++ src/main/java/com/tinylang/utils/Scanner.java
@@ -2,77 +2,120 @@ package com.tinylang.utils;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 public class Scanner {
-    private FileReader in;
+    private java.util.Scanner in;
     private STATE currState;
-    private int ASCII;
-    private final static HashMap<String, String> special_chars = new HashMap<>() {{
-        special_chars.put(";", "SEMICOLON");
-        special_chars.put(":=", "ASSIGN");
-        special_chars.put("<", "LESSTHAN");
-        special_chars.put("=", "EQUAL");
-        special_chars.put("+", "PLUS");
-        special_chars.put("-", "MINUS");
-        special_chars.put("*", "MULT");
-        special_chars.put("/", "DIV");
-        special_chars.put("(", "OPENBRACKET");
-        special_chars.put(")", "CLOSEDBRACKET");
-    }};
-    private final static HashMap<String, String> Reserved_keyword = new HashMap<>() {{
-        Reserved_keyword.put("if", "IF");
-        Reserved_keyword.put("then", "THEN");
-        Reserved_keyword.put("end", "END");
-        Reserved_keyword.put("repeat", "REPEAT");
-        Reserved_keyword.put("until", "UNTIL");
-        Reserved_keyword.put("read", "READ");
-        Reserved_keyword.put("write", "WRITE");
-    }};
+    char currChar = ' ';
+    boolean isDelimiter = false;
+    private final static Map<String, String> special_chars = Map.ofEntries(
+    entry(";", "SEMICOLON"),
+    entry(":=", "ASSIGN"),
+    entry("<", "LESSTHAN"),
+    entry("=", "EQUAL"),
+    entry("+", "PLUS"),
+    entry("-", "MINUS"),
+    entry("*", "MULT"),
+    entry("/", "DIV"),
+    entry("(", "OPENBRACKET"),
+    entry(")", "CLOSEDBRACKET")
+    );
+    private final static Map<String, String> Reserved_keyword = Map.ofEntries(
+        entry("if", "IF"),
+        entry("then", "THEN"),
+        entry("end", "END"),
+        entry("repeat", "REPEAT"),
+        entry("until", "UNTIL"),
+        entry("read", "READ"),
+        entry("write", "WRITE")
+    );
     private enum STATE {
-        START,  // Starting state
-        IN_ID,  // Indicates reading an identifier
-        IN_NUM, // Indicates reading a numeric character
-        ERROR,  // Indictes error state
+        START,
+        IN_ALPHA,
+        IN_NUM,
+        IN_ASSIGN,
+        IN_COMMENT,
+        ERROR,
         DONE,
         EOF
     }
 
     public Scanner(String file_path){
         try{
-            in = new FileReader(file_path);
+            in = new java.util.Scanner(new File(file_path));
             currState = STATE.START;
-            ASCII = -1;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     TokenRecord getToken() throws IOException {
-        ASCII = in.read();
-        char currChar;
-        StringBuilder TokenVal = new StringBuilder();
-        if(ASCII == -1) {
-            currState = STATE.EOF;
+        if(currState == STATE.EOF || currState == STATE.ERROR) {
             return null;
         }
+        StringBuilder TokenVal = new StringBuilder("");
+        while(in.hasNext() && currState != STATE.DONE) {
+            if(!isDelimiter) {
+                in.useDelimiter("");
+                currChar = in.next().charAt(0);
+            }
+            isDelimiter = false;
+            if (currState != STATE.IN_ASSIGN && currState != STATE.IN_COMMENT && special_chars.containsKey(Character.toString(currChar)) && TokenVal.length() == 0) {
+                currState = STATE.START;
+                return new TokenRecord(Character.toString(currChar), special_chars.get(Character.toString(currChar)));
+            } else if (currState != STATE.IN_ASSIGN && currState != STATE.IN_COMMENT && special_chars.containsKey(Character.toString(currChar))){
+                isDelimiter = true;
+                currState = STATE.DONE;
+            }
+            // ---------------------------------------------------------------
+            else if (currChar == '{'){
+                currState = STATE.IN_COMMENT;
+            } else if (currChar == '}' && currState == STATE.IN_COMMENT) {
+                currState = STATE.START;
+            } else if (currState == STATE.IN_COMMENT || ((currChar == ' ' || currChar == '\n') && TokenVal.length() == 0)){
+                /* DO NOTHING - SKIP */
+            } else if ((currChar == ' ' || currChar == '\n' ) && !TokenVal.equals("")) {
+                currState = STATE.DONE;
+            } else if (currChar == ':') {
+                TokenVal.append(currChar);
+                currState = STATE.IN_ASSIGN;
+            } else if (currChar != '=' && currState == STATE.IN_ASSIGN) {
+                currState = STATE.ERROR;
+                break;
+            } else if(currChar == '=' && currState == STATE.IN_ASSIGN) {
+                currState = STATE.START;
+                TokenVal.append(currChar);
+                return new TokenRecord(TokenVal.toString(), "ASSIGN");
+            } else if(Character.isAlphabetic(currChar) && currState != STATE.IN_NUM) {
+                TokenVal.append(currChar);
+                currState = STATE.IN_ALPHA;
+            } else if(Character.isDigit(currChar)) {
+                TokenVal.append(currChar);
+                if(currState != STATE.IN_ALPHA) currState = STATE.IN_NUM;
+            } else {
+                currState = STATE.ERROR;
+                System.out.println("Error: Input code is incorrect");
+                break;
+            }
+        }
 
-        while(ASCII != -1 && currState != STATE.DONE) {
-            currChar = (char)ASCII;
-            TokenVal.append(currChar);
-
-            ASCII = in.read();
+        if(!in.hasNext()) {
+            currState = STATE.EOF;
+        } else if (currState != STATE.ERROR) {
+            currState = STATE.START;
         }
 
         String TokenType;
-        if(Reserved_keyword.containsKey(TokenVal.toString()))
+        if(TokenVal.length() > 0 && Reserved_keyword.containsKey(TokenVal.toString()))
             TokenType = Reserved_keyword.get(TokenVal.toString());
-        else if(special_chars.containsKey(TokenVal.toString()))
-            TokenType = special_chars.get(TokenVal.toString());
-        else if(Character.isDigit(TokenVal.charAt(0))){
+        else if(TokenVal.length() > 0 && Character.isDigit(TokenVal.charAt(0))){
             TokenType = "NUMBER";
         } else {
             TokenType = "IDENTIFIER";
         }
-        return new TokenRecord(TokenType, TokenVal);
+        return new TokenRecord(TokenVal.toString(), TokenType);
     }
 }
