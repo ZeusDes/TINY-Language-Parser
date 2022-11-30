4552d6ca9d19dbfd1cc1bf0ac11643e548c8163a
Mustafa Amer <Eng.MustafaAmer0@gmail.com>
2022-11-30 08:33:55 +0200

    Adding status label

--- src/main/java/com/example/mytinylanguageparser/GUIController.java
+++ src/main/java/com/example/mytinylanguageparser/GUIController.java
@@ -3,6 +3,7 @@ package com.example.mytinylanguageparser;
 import java.io.File;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -14,12 +15,18 @@ public class GUIController {
     private File outputFile;
 
     @FXML
+    private Label status;
+    @FXML
+    private Button compile;
+
+    @FXML
     protected void compile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Save Output");
         outputFile = fileChooser.showSaveDialog(new Stage());
         try{
             ScannerDriver.scan(inputFile, outputFile);
+            status.setText("Compiled!");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,5 +37,7 @@ public class GUIController {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open TINY File Code");
         inputFile = fileChooser.showOpenDialog(new Stage());
+        compile.setDisable(false);
+        status.setText("File Opened");
     }
 }
