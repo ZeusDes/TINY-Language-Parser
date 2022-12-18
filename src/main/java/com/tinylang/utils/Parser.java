bcbaa481f8f84add1d76774ac3a3ec22df3d8ab3
ZeusDes <mohamed.ahmed.ma19.ma@gmail.com>
2022-12-18 04:42:13 +0200

    Add Implementation to some Parser functions

--- src/main/java/com/tinylang/utils/Parser.java
+++ src/main/java/com/tinylang/utils/Parser.java
@@ -1,13 +1,17 @@
 package com.tinylang.utils;
 import com.tinylang.utils.Exceptions.ParserException;
 import com.tinylang.utils.Exceptions.ScannerException;
+import guru.nidi.graphviz.attribute.Label;
+import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.model.MutableNode;
 
 import java.io.IOException;
+
+import static guru.nidi.graphviz.model.Factory.mutNode;
 
 public class Parser {
     Scanner scanner;
     TokenRecord currToken;
-    /* ParseTree tree; */
     public Parser(String file_path) throws ScannerException, IOException {
         scanner = new Scanner(file_path);
         currToken = scanner.getToken();
@@ -15,11 +19,81 @@ public class Parser {
 
     // FIXME: Handling Parser Exception in GUI
     boolean match(String target) throws ParserException, ScannerException, IOException {
-        String tokenVal = currToken.getStringValue();
+        String tokenVal = currToken.getTokenType();
         if(!tokenVal.equals(target)) {
             throw new ParserException(tokenVal);
         }
         currToken = scanner.getToken();
         return true;
     }
+
+    MutableNode addop() throws ParserException, ScannerException, IOException {
+        MutableNode node;
+        String lbl = "<b>OPERATOR</b></br>";
+        if(currToken.getTokenType().equals("PLUS")){
+            node = mutNode("+").add(Label.html(lbl + "(+)"));
+            match("PLUS");
+        } else if(currToken.getTokenType().equals("MINUS")){
+            node = mutNode("-").add(Label.html(lbl + "(-)"));
+            match("MINUS");
+        } else {
+            throw new ParserException(currToken.getStringValue());
+        }
+        return node;
+    }
+
+    MutableNode mulop() throws ParserException, ScannerException, IOException {
+        MutableNode node;
+        String lbl = "<b>OPERATOR</b></br>";
+        if(currToken.getTokenType().equals("MULT")){
+            node = mutNode("*").add(Label.html(lbl + "(*)"));
+            match("MULT");
+        } else if(currToken.getTokenType().equals("/")){
+            node = mutNode("DIV").add(Label.html(lbl + "(/)"));
+            match("DIV");
+        } else {
+            throw new ParserException(currToken.getStringValue());
+        }
+        return node;
+    }
+
+    MutableNode comparison_op() throws ParserException, ScannerException, IOException {
+        MutableNode node;
+        String lbl = "<b>OPERATOR</b></br>";
+        if(currToken.getTokenType().equals("LESSTHAN")){
+            node = mutNode("<").add(Label.html(lbl + "(<)"));
+            match("LESSTHAN");
+        } else if(currToken.getTokenType().equals("EQUAL")){
+            node = mutNode("=").add(Label.html(lbl + "(=)"));
+            match("EQUAL");
+        } else {
+            throw new ParserException(currToken.getStringValue());
+        }
+        return node;
+    }
+
+    MutableNode factor(){
+        MutableNode node = null;
+        /* Code */
+        return node;
+    }
+
+    MutableNode term(){
+        MutableNode node = null;
+        /* Code */
+        return node;
+    }
+
+    MutableNode statement(){
+        MutableNode node = null;
+        /* Code */
+        return node;
+    }
+
+    MutableNode stmt_seq(){
+        MutableNode node = null;
+        /* Code*/
+        return node;
+    }
+
 }
