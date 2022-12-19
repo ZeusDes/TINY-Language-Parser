a2d2d6cf1ba086ce249e17937a7cb1039d2c02be
ZeusDes <mohamed.ahmed.ma19.ma@gmail.com>
2022-12-19 04:52:33 +0200

    Add Parse Button

--- src/main/java/com/tinylang/GUIController.java
+++ src/main/java/com/tinylang/GUIController.java
@@ -3,7 +3,9 @@ package com.tinylang;
 import java.io.File;
 import java.io.IOException;
 
+import com.tinylang.utils.Exceptions.ParserException;
 import com.tinylang.utils.Exceptions.ScannerException;
+import com.tinylang.utils.Parser;
 import com.tinylang.utils.ScannerDriver;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,6 +19,7 @@ public class GUIController {
     private File inputFile;
     private File outputFile;
 
+    private String dot;
     @FXML
     private Label status;
     @FXML
@@ -45,5 +48,11 @@ public class GUIController {
         inputFile = fileChooser.showOpenDialog(new Stage());
         compile.setDisable(false);
         status.setText("File Opened");
+    }
+
+    @FXML
+    protected void parseController() throws ScannerException, IOException, ParserException {
+        Parser parser = new Parser(inputFile.getAbsolutePath());
+        dot = parser.parse();
     }
 }
